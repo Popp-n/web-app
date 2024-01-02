0fd93686eb8dae71d759092c70c0c16135a3320c
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-01-02 13:19:18 +0100

    feat: about-you in progress

--- src/Components/Pages/AboutYou/AboutYou.tsx
+++ src/Components/Pages/AboutYou/AboutYou.tsx
@@ -15,20 +15,15 @@ const AboutYou: React.FC<Props> = () => {
     <OnboardingTemplate>
       <>
         <Left>
-          <Typography as="h3" className="h-18" text="Join the" />
-          <Typography as="h5" className="mt-10 h-19">
-            <>
-              <img
-                src="/assets/images/poppn-logo-animation.gif"
-                alt=""
-                className="image-text"
-              />{" "}
-              Revolution
-            </>
-          </Typography>
+          <Typography
+            as="h3"
+            className="h-18 max-width-350"
+            text="Tell us about yourself"
+          />
+
           <Typography
             as="p"
-            className="p-16 mt-60"
+            className="p-16 mt-20"
             text="If you already have an account"
           />
           <Typography as="p" className="p-16 mt-5">
@@ -38,15 +33,13 @@ const AboutYou: React.FC<Props> = () => {
           </Typography>
         </Left>
         <Right>
-          <Typography as="h5" className="h-20 mb-30" text="Sign Up" />
-          <Input placeholder="First Name" />
-          <Input placeholder="Last Name" />
-          <Input placeholder="Email Address" type="email" />
-          <Input placeholder="Password" type="password" noMargin />
+          <Input placeholder="Birthday (DD/MM/YYYY)" />
+          <Input placeholder="User Name" />
+          <Input placeholder="Country" />
+          <Input placeholder="State" />
+          <Input placeholder="Phone Number" noMargin />
 
-          <Button className="btn-full btn-4 b-5 btn-md mt-40 ff">
-            Register
-          </Button>
+          <Button className="btn-full btn-4 b-5 btn-md mt-40 ff">Next</Button>
         </Right>
       </>
     </OnboardingTemplate>
